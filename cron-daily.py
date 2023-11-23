55d56377ae8257ed65940bdd9ad37dea712b1bf3
vikascnr <vikassinghcnr@gmail.com>
2023-11-23 21:22:45 +0530

    Update cron-daily.py

--- cron-daily.py
+++ cron-daily.py
@@ -6,21 +6,11 @@ create_article = requests.get("/api/create-article-list")
 revalidate = requests.get("/api/revalidate/invoke/all")
 
 curr_time = datetime.now()
-#HOUR        = datetime.now().hour   # the current hour
-#MINUTE      = datetime.now().minute
-HOUR=4
-MINUTE=29
-
-print(type(HOUR))
-print(HOUR)
-print(MINUTE)
-
 while (HOUR==4 and MINUTE <= 30):
     print("Going to sleep for 60 seconds")
     time.sleep(60)
 
 schedular_start = requests.get("/api/scheduler/start")
-
 
 print(create_article.text)
 print(revalidate.text)
